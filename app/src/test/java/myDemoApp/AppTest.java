6c4ec3b6a472689039cfde21a2945fb70525775e
hiracansu <hiracansudegirmenbasi@gmail.com>
2022-03-15 00:07:10 +0300

    deneme

--- app/src/test/java/myDemoApp/AppTest.java
+++ app/src/test/java/myDemoApp/AppTest.java
@@ -9,31 +9,31 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
-    @Test
-    public void testFound() {
-       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-       assertTrue(App.search(array, 4));
-     }
+    //Test void appHasAGreeting() {
+    //   App classUnderTest = new App();
+    //   assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    //
+    //Test
+    //ublic void testFound() {
+    //  ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+    //  assertTrue(App.sub(array, 4,5));
+    //}
  
-     @Test
-     public void testNotFound() {
-       ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-       assertFalse(App.search(array, 5));
-     }
+    //@Test
+    //public void testNotFound() {
+    //  ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+    //  assertFalse(App.sub(array, 5));
+    //}
  
-     @Test
-     public void testEmptyArray() {
-       ArrayList<Integer> array = new ArrayList<>();
-       assertFalse(App.search(array, 1));
-     }
+    //@Test
+    //public void testEmptyArray() {
+    //  ArrayList<Integer> array = new ArrayList<>();
+    //  assertFalse(App.search(array, 1));
+    //}
  
-     @Test
-     public void testNull() {
-       assertFalse(App.search(null, 1));
-     }
+    //@Test
+    //public void testNull() {
+    //  assertFalse(App.search(null, 1));
+    //}
  
 }
